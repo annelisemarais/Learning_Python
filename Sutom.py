7e37ad05719e344159f87b9f0e09935cb9f07c28
annelisemarais <maraisannelise98@gmail.com>
2022-09-19 17:39:46 +0200

    Update Sutom.py

--- Sutom.py
+++ Sutom.py
@@ -23,15 +23,13 @@ word = random.choice(list(allwords))
 length = len(word)
 
 dispword = '_ ' * length
+dispwordlist = list('_'*length)
 
-print(rules)
-print('')
+
+print(rules+ '\n')
 print("The word you are looking for has {} letters, best of luck !".format(length))
 print(dispword)
 
-dispplayerchoice = '_ ' * length
-
-playerchoice = input("")
 
 def wordcheck(playerchoice):
 	if len(playerchoice) != length:
@@ -43,33 +41,32 @@ def wordcheck(playerchoice):
 
 def wordcomparison(playerchoice, word):
 	index = 0
+	print('You chose:')
 	for letter in playerchoice:
 		if letter not in word:
-			print( 'Wrong letter: 'Fore.RED + letter)
-			print(Style.RESET_ALL)
+			print(Fore.RED + letter, end =" ")
+			print(Style.RESET_ALL, end =" ")
 		else:
 			if playerchoice[index] == word[index]:
-				print(Fore.GREEN + letter)
-				print(Style.RESET_ALL)
+				dispwordlist[index] = letter
+				print(Fore.GREEN + letter, end =" ")
+				print(Style.RESET_ALL, end =" ")
 			else:
-				print(Fore.YELLOW + letter)
-				print(Style.RESET_ALL)
-		index = +1
+				print(Fore.YELLOW + letter, end =" ")
+				print(Style.RESET_ALL, end =" ")
+		index = index+1
+	word2find = ''.join(dispwordlist)
+	print('\n')
+	print('Word to find:\n')
+	for letter in word2find:
+		print(letter, end =" ")
 
+playerchoice = ''
 
-
-
-index = 0
-for letter in playerchoice:
-	if letter not in word:
-		dispplayerchoice[index] = letter
+while True:
+	if playerchoice != word:
+		playerchoice = input("")
+		wordcheck(playerchoice)
+		wordcomparison(playerchoice,word)
 	else:
-		if playerchoice[index] == word[index]:
-			dispplayerchoice[index] = letter
-			dispword[index] = letter
-		else:
-			dispplayerchoice[index] = letter
-			dispword[index] = letter
-	index = +1
-
-	dispplayerchoice[index].replace("_ ", letter)
+		print('Bravo')
